3cd4c9764a551bf83459e4c8026cff574b7e325f
Chandleroca <106095978+ChandlerIlagan@users.noreply.github.com>
2025-10-17 17:06:51 +0800

    movement update

--- Assets/Scripts/Core/Player/Interfaces/IPlayerMovement.cs
+++ Assets/Scripts/Core/Player/Interfaces/IPlayerMovement.cs
@@ -3,7 +3,7 @@ using UnityEngine;
 public interface IPlayerMovement
 {
     public bool CanMove { get; }
-    IPlayerMovement Move2DRigid(Rigidbody2D gameObject);
+    IPlayerMovement Move2DRigid(Rigidbody2D body);
     IPlayerMovement WithSpeed(float speed);
     public void EnableMovement();
     public void DisableMovement();
--- Assets/Scripts/Core/Player/PlayerDefaultMovement.cs
+++ Assets/Scripts/Core/Player/PlayerDefaultMovement.cs
@@ -9,6 +9,7 @@ public class PlayerDefaultMovement : MonoBehaviour, IPlayerMovement
     private bool _canMove;
     private bool _isMoving;
     private float _moveSpeed = 0;
+    private Vector2 _movementOffset;
     private InputSystem_Actions _inputSystem;
     
     public void Initialize(InputSystem_Actions inputSystem)
@@ -22,21 +23,26 @@ public class PlayerDefaultMovement : MonoBehaviour, IPlayerMovement
 
     private void OnMovePressed(InputAction.CallbackContext context)
     {
+        Vector2 input = context.ReadValue<Vector2>();
         _isMoving = true;
+        
+        _movementOffset = input;
     }
     private void OnMoveReleased(InputAction.CallbackContext context)
     {
         Vector2 input = context.ReadValue<Vector2>();
         if (input == Vector2.zero)
             _isMoving = false;
+        
+        _movementOffset = input;
     }
 
-    public IPlayerMovement Move2DRigid(Rigidbody2D gameObject)
+    public IPlayerMovement Move2DRigid(Rigidbody2D body)
     {
         if (!_canMove)
             return this;
 
-        
+        body.linearVelocity = _movementOffset * _moveSpeed;
         
         return this;
     }
