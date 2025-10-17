039640ee1ac8e1b67b8fc5a8f09a9f9b06db92fd
Chandleroca <106095978+ChandlerIlagan@users.noreply.github.com>
2025-10-17 18:07:19 +0800

    shooting update

--- Assets/Scripts/Core/Player/PlayerDefaultWeaponController.cs
+++ Assets/Scripts/Core/Player/PlayerDefaultWeaponController.cs
@@ -1,25 +1,50 @@
 using System;
+using System.Collections;
 using UnityEngine;
 using UnityEngine.InputSystem;
+using Utilities;
 
 public class PlayerDefaultWeaponController : MonoBehaviour, IPlayerWeaponController
 {
+    private const int _initialBulletCount = 30;
+    
     [SerializeField] private PlayerWeaponSettingsSO _initialWeaponSetting;
     [SerializeField] private GameObject _bulletPrefab;
     
     private bool _canShoot;
     private InputSystem_Actions _inputSystem;
     private PlayerWeaponSettingsSO _currentWeaponSettings;
+    private Pool.GameObj _bulletPool;
+    private Coroutine _shootingRoutine;
     
     public void Initialize(InputSystem_Actions inputSystem, PlayerSettingsSO playerSettings)
     {
+        _shootingRoutine = null;
+        _bulletPool = new Pool.GameObj(_initialBulletCount ,_bulletPrefab, transform);
         ChangeWeapon(_initialWeaponSetting);
         
         _inputSystem = inputSystem;
-        _inputSystem.Player.Shoot.performed += OnShoot;
+        _inputSystem.Player.Shoot.performed += OnShootStart;
+        _inputSystem.Player.Shoot.canceled += OnShootStop;
     }
 
-    private void OnShoot(InputAction.CallbackContext obj)
+    private void OnShootStart(InputAction.CallbackContext obj)
+    {
+        if (_shootingRoutine != null)
+            StopCoroutine(_shootingRoutine);
+        
+        _shootingRoutine = StartCoroutine(ShootingBehavior());
+    }
+    
+    private void OnShootStop(InputAction.CallbackContext obj)
+    {
+        if (_shootingRoutine != null)
+            StopCoroutine(_shootingRoutine);
+        
+        _shootingRoutine = null;
+    }
+
+    private IEnumerator ShootingBehavior()
     {
         
     }
