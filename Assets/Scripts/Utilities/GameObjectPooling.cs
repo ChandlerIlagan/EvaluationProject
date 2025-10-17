5415a562e1e9702cff54131ef7a7ede10416aac8
Chandleroca <106095978+ChandlerIlagan@users.noreply.github.com>
2025-10-17 14:46:54 +0800

    Update

--- Assets/Scripts/Utilities/GameObjectPooling.cs
+++ Assets/Scripts/Utilities/GameObjectPooling.cs
@@ -10,10 +10,11 @@ namespace Utilities
 {
     /// <summary>
     /// Lightweight-object-pooling where it creates disabled instances
-    /// Will get first index[0] if disabled using Get()
-    /// else will create new instance
-    /// Gotten instances will push itself to last index
-    /// Disabled instances will push itself to first index
+    /// Highly recommended to initialize on Awake()
+    ///     > Will get first index[0] if disabled using Get()
+    ///     > else will create new instance
+    ///     > Gotten instances will push itself to last index
+    ///     > Disabled instances will push itself to first index
     /// </summary>
     public static class Pool
     {
@@ -24,23 +25,44 @@ namespace Utilities
           private List<GameObject> _objPool;
           private GameObject _referenceObjInstance;
               
-          public GameObj(int size, GameObject obj)
+          public GameObj(int size, GameObject obj, Transform parent)
           {
               _objPool = new List<GameObject>();
               _referenceObjInstance = obj;
               
               for (int x = 0; x < size; x++)
-                  AddNewObjInstance();
+              {
+                  GameObject newObj = AddNewObjInstance();
+                  
+                  if (parent != null)
+                      newObj.transform.parent = parent;
+              }
+          }
+          
+          public GameObj(GameObject obj, Transform parent) : this(_defaultInitialPoolSize, obj, parent) {}
+          public GameObj(GameObject obj) : this(_defaultInitialPoolSize, obj, null) {}
+
+          private GameObject AddNewObjInstance()
+          {
+              GameObject newObj = UnityEngine.Object.Instantiate(_referenceObjInstance);
+              _objPool.Add(newObj);
+              return newObj;
+          }
+              
+          public GameObject Get(bool isActiveOnGet)
+          {
+              GameObject objectToReturn;
+              
+              if (!_objPool[0].activeInHierarchy)
+                  objectToReturn = _objPool[0];
+              else
+                  objectToReturn = AddNewObjInstance();
+
+              objectToReturn.SetActive(isActiveOnGet);
+              return objectToReturn;
           }
 
-          private void AddNewObjInstance() => _objPool.Add(UnityEngine.Object.Instantiate(_referenceObjInstance));
-          public GameObj(GameObject obj) : this(_defaultInitialPoolSize, obj) {}
-              
-          // Gets first index if available, else, create new instance
-          public GameObject Get()
-          {
-              if ()
-          }
+          public GameObject Get() => Get(true);
       }
     }
 }
