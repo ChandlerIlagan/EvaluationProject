4d7ca09a3ec72bddd978bd795d4d7dd303b05bf8
Chandleroca <106095978+ChandlerIlagan@users.noreply.github.com>
2025-10-17 17:16:14 +0800

    Setup

---
+++ Assets/Scripts/Core/Player/Interfaces/IPlayerWeaponController.cs
@@ -0,0 +1,8 @@
+using UnityEngine;
+
+public interface IPlayerWeaponController
+{
+    public void Initialize();
+    public void EnableWeapons();
+    public void DisableWeapons();
+}
---
+++ Assets/Scripts/Core/Player/PlayerDefaultWeaponController.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+
+public class PlayerDefaultWeaponController : MonoBehaviour, IPlayerWeaponController
+{
+    public void Initialize()
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public void EnableWeapons()
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public void DisableWeapons()
+    {
+        throw new System.NotImplementedException();
+    }
+}
